5d765cfa382e4c47c5ba01699246b766cb227207
SarthakJShetty <sarthakshetty97@gmail.com>
2018-06-16 18:45:22 +0530

    Adding updated code here.

--- PERT.py
+++ PERT.py
@@ -14,38 +14,45 @@
 #Importing required libraries over here
 import numpy as np
 import matplotlib.pyplot as plt
+import os
+from graphviz import Digraph
 
-#Definung the necessary functions here
+#Declaring the graph here from Graphviz
+def graph_define():
+	nameOfGraph=input("[INFO] Provide a name for the graph: ")
+	g=Digraph('G',filename=nameOfGraph+'.csv',engine='sfdp')
+	g.attr('node',shape='circle')
+	return g
 
 #Function to determine the number of nodes
 def number_of_nodes():
-	n=int(input("Enter the number of nodes involved in the network: "))
+	n=int(input("\n[DATA] Enter the number of nodes involved in the network: "))
 	return n
 
 #Function to determine the number of events between the nodes
 def number_of_events(a,n):
-	print("The number of events between the nodes is a total of %d"%(n-1))
+	print("[INFO] The number of events between the nodes is a total of %d"%n)
 	return (n-1)
 
 #Assigns the preceeding activity
 def relation(a,n):
-	print("Note: The first node is considered the starting event with node index 0\n")
+	print("[INFO] The first node is considered the starting event with node index 0\n")
 	#Assigning first node to -1 to indicate start of the network
-	a[0][0]=-1
-	print("Note: The event with index %d is considered the last event with node index %d\n"%(n,n))
+	a[0][0]=0
+	print("[INFO] The event with index %d is the last event"%(n-1))
 	for i in range(1,n):
-		a[i][0]=int(input("Enter the preceding node for %d node: "%(i+1)))
+		a[i][0]=int(input("\n[DATA] Enter the preceding node for %d node: "%(i)))
 
 #Explains the relation between the nodes
 def explainer(a,n):
-	print("Node: 1 is the starting node")
+	print("[INFO] Node: 1 is the starting node")
 	for i in range(1,n):
-		print("Node: %d follows Node: %d"%(1+i,int(a[i][0])+1))
+		print("[INFO] Node: %d follows Node: %d"%(i+1,int(a[i][0])+1))
 
 #Defining the event time between the nodes, relative in nature
 def rel_events(a,n):
 	for i in range(1,n):
-		a[i][1]=input("Enter the relative time for %d and %d event to take place:"%(i+1,a[i][0]))
+		a[i][1]=input("[DATA] Enter the relative time for %d and %d event to take place:"%(i,a[i][0]))
 
 #Calculating the absolute time between with respect to the starting point
 def abs_events(a,n):
@@ -59,7 +66,7 @@ def last_node(a,n):
 	for i in range(0,n):
 		if(a[i][2]>a[j][2]):
 			j=i
-	print("The last node in the network is: %d"%(j))
+	print("[INFO] The last node in the network is: %d"%(j))
 	return j
 
 #To find the back time
@@ -70,7 +77,18 @@ def back_time(a,n,j):
 	for i in range(n):
 		k=int(a[j][0])
 
+#We'll be using graphviz to build the graph
+def graph_builder(a,g,n):
+	#Changing the attributes of the 'node' 0, to ensure that starting node is marked by double-circle
+	g.attr('node',shape='doublecircle')
+	g.node('0')
+	g.attr('node',shape='circle')
+	for i in range(1,len(a)):
+		g.edge(str(int(i)),str(int(a[i][0])),label=str(a[i][2]),labeltooltip="Time taken for event"+str(a[i][2]))
 
+#This function uses 
+def graph_plotter(g):
+	g.view()
 
 #n is a variable to store the number of nodes involved in the network
 n=0
@@ -105,8 +123,18 @@ j=0
 j=last_node(a,n)
 print("\n")
 
+#Provide a name for the graph that will be generated.
+g=graph_define()
+
+#Builds the graph. Graph is then sent for viewing
+graph_builder(a,g,n)
+
+#Plots the graph
+graph_plotter(g)
 
 
 #Printing the final matrix obtained
-print("First row provides the node leading upto respective node.\nSecond row provides relative time elapsed between said node and node in first row.\nThird row provides the absolute time taken for event completion.\n")
+print("[INFO] First row provides the node leading upto respective node.\n")
+print("[INFO] Second row provides relative time elapsed between said node and node in first row.\n")
+print("[INFO] Third row provides the absolute time taken for event completion.\n")
 print(a)
