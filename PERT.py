56d86c7764d1e912560726425d02180258281a12
SarthakJShetty <sarthakshetty97@gmail.com>
2018-06-25 16:33:00 +0530

    Improved formatting, improved index

--- PERT.py
+++ PERT.py
@@ -22,55 +22,63 @@ from graphviz import Digraph
 #Declaring the graph here from Graphviz
 def graph_define():
 	nameOfGraph=input("[INFO] Provide a name for the graph: ")
-	path='Results'
+	#Stores the nameOfGraph.pdf, nameOfGraph.csv files in the nameOfGraph folder under Results.
+	path='Results/'+nameOfGraph
 	processGraph=Digraph('G',filename=os.path.join(path,nameOfGraph+'.csv'),engine='sfdp')
 	processGraph.attr('node',shape='circle')
 	return processGraph
 
 #Function to determine the number of nodes
 def number_of_nodes():
-	numberOfNodes=int(input("\n[DATA] Enter the number of nodes involved in the network: "))
+	try:
+		numberOfNodes=int(input("\n[DATA] Enter the number of nodes involved in the network: "))
+	except ValueError:
+		numberOfNodes=int(input("\n[DATA] Enter a valid number of nodes involved in the network: "))
 	return numberOfNodes
 
 #Function to determine the number of events between the nodes
 def number_of_events(tableOfEvents,numberOfNodes):
+	#Prints the serial number of events on the extreme left.
+	for node in range(0,numberOfNodes):
+		tableOfEvents[node][0]=node
 	print("[INFO] The number of events between the nodes is a total of %d"%numberOfNodes)
 	return (numberOfNodes-1)
 
 #Assigns the preceeding activity
 def event_relation(tableOfEvents,numberOfNodes):
-	print("[INFO] The first node is considered the starting event with node index 0\n")
+	print("[INFO] The first node is considered the starting node with node index 0")
 	#Assigning first node to -1 to indicate start of the network
 	tableOfEvents[0][0]=0
-	print("[INFO] The event with index %d is the last event"%(numberOfNodes-1))
-	for i in range(1,numberOfNodes):
-		tableOfEvents[i][0]=int(input("\n[DATA] Enter the preceding node for %d node: "%(i)))
+	print("[INFO] The event with index %d is the last event\n"%(numberOfNodes-1))
+	for node in range(0,numberOfNodes):
+		if(node!=0):
+			tableOfEvents[node][1]=int(input("[DATA] Enter the preceding node for %d node: "%(node)))
 
 #Explains the relation between the nodes
 def graph_explainer(tableOfEvents,numberOfNodes):
 	print("[INFO] Node: 1 is the starting node")
-	for i in range(1,numberOfNodes):
-		print("[INFO] Node: %d follows Node: %d"%(i+1,int(tableOfEvents[i][0])+1))
+	for node in range(1,numberOfNodes):
+		print("[INFO] Node: %d follows Node: %d"%(node,int(tableOfEvents[node][1])+1))
 
 #Defining the event time between the nodes, relative in nature
 def rel_events(a,numberOfNodes):
-	for i in range(1,numberOfNodes):
-		tableOfEvents[i][1]=input("[DATA] Enter the relative time for %d and %d event to take place:"%(i,tableOfEvents[i][0]))
+	for node in range(1,numberOfNodes):
+		tableOfEvents[node][2]=input("[DATA] Enter the relative time for %d and %d event to take place:"%(node,tableOfEvents[node][1]))
 
 #Calculating the absolute time between with respect to the starting point
 def abs_events(tableOfEvents,numberOfNodes):
-	for i in range(1,numberOfNodes):
-		j=int(tableOfEvents[i][0])
-		tableOfEvents[i][2]=tableOfEvents[i][1]+tableOfEvents[j][2]
+	for node in range(1,numberOfNodes):
+		node=int(tableOfEvents[node][1])
+		tableOfEvents[node][3]=tableOfEvents[node][2]+tableOfEvents[node][3]
 
 #To find the last node in the network
 def last_node(tableOfEvents,numberOfNodes):
-	j=0
-	for i in range(0,numberOfNodes):
-		if(tableOfEvents[i][2]>tableOfEvents[j][2]):
-			j=i
-	print("[INFO] The last node in the network is: %d"%(j))
-	return j
+	for node in range(0,numberOfNodes):
+		referenceNode=0
+		if(tableOfEvents[node][3]>tableOfEvents[referenceNode][3]):
+			referenceNode=node
+	print("[INFO] The last node in the network is: %d"%(referenceNode))
+	return referenceNode
 
 #We'll be using graphviz to build the graph
 def graph_builder(tableOfEvents,processGraph,numberOfNodes):
@@ -78,15 +86,16 @@ def graph_builder(tableOfEvents,processGraph,numberOfNodes):
 	processGraph.attr('node',shape='doublecircle')
 	processGraph.node('0')
 	processGraph.attr('node',shape='circle')
-	for i in range(1,len(tableOfEvents)):
-		processGraph.edge(str(int(tableOfEvents[i][0])),str(int(i)),label=("Forward Pass Time: "+str(int(tableOfEvents[i][2]))))
+	for node in range(1,len(tableOfEvents)):
+		processGraph.edge(str(int(tableOfEvents[node][1])),str(int(node)),label=("Forward Pass Time: "+str(int(tableOfEvents[node][3]))))
 		
 		#Backward time not yet implemented
-		#g.edge(str(int(i)),str(int(a[i][0])),label="Backward Pass Time: "+str(a[i][3]))
+		#g.edge(str(int(node)),str(int(a[node][0])),label="Backward Pass Time: "+str(a[node][3]))
 
 #This function uses 
 def graph_plotter(processGraph):
 	processGraph.view()
+	print('\n')
 
 #Analyzes the nodes in reverse to detwemine backward-pass time.
 '''def backward_pass(a,g,n):
@@ -98,15 +107,12 @@ def graph_plotter(processGraph):
 		a[events_1][3]=np.amax(b)
 	print(b)'''
 
-
-
 #n is a variable to store the number of nodes involved in the network
-numberOfNodes=0
 numberOfNodes=number_of_nodes()
 print("\n")
 
 #Declaring the numpy array here for entering the nodes
-tableOfEvents=np.zeros((numberOfNodes,4))
+tableOfEvents=np.zeros((numberOfNodes,5))
 
 #Defines the relation of connectivity of the nodes
 event_relation(tableOfEvents,numberOfNodes)
@@ -122,14 +128,12 @@ print("\n")
 
 #Function to enter the time elapsed in carrying out the events
 rel_events(tableOfEvents,numberOfNodes)
-print("\n")
 
 #Function to evaluate the absolute time in execution of the events
 abs_events(tableOfEvents,numberOfNodes)
 print("\n")
 
 #Prints the last node in the network
-j=0
 j=last_node(tableOfEvents,numberOfNodes)
 print("\n")
 
@@ -145,9 +149,9 @@ graph_builder(tableOfEvents,processGraph,numberOfNodes)
 #Plots the graph
 graph_plotter(processGraph)
 
-
 #Printing the final matrix obtained
-print("[INFO] First row provides the node leading upto respective node.\n")
-print("[INFO] Second row provides relative time elapsed between said node and node in first row.\n")
-print("[INFO] Third row provides the absolute time taken for event completion.\n")
+print("[INFO] First row provides a serial reference for the events")
+print("[INFO] Second row provides the node leading upto respective node.")
+print("[INFO] Third row provides relative time elapsed.")
+print("[INFO] Fourth row provides the absolute time taken for event completion.\n")
 print(tableOfEvents)
