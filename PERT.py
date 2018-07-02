456c89d02ca17ef69a9d5a8a24092863c83b843e
SarthakJShetty <sarthakshetty97@gmail.com>
2018-07-02 17:19:27 +0530

    Fixed the absolute time and relative time issue

--- PERT.py
+++ PERT.py
@@ -11,7 +11,7 @@
 #Incorporated the last node concept.
 #Yet to incorporate reverse times.
 
-#Checkout the repository README for detailed log.
+#Checkout the repository README for a more detailed log.
 
 #Importing required libraries over here
 import numpy as np
@@ -52,7 +52,14 @@ def event_relation(tableOfEvents,numberOfNodes):
 	print("[INFO] The event with index %d is the last event\n"%(numberOfNodes-1))
 	for node in range(0,numberOfNodes):
 		if(node!=0):
-			tableOfEvents[node][1]=int(input("[DATA] Enter the preceding node for %d node: "%(node)))
+			tableOfEvents[node][1]=int(input("[DATA] Enter the preceding node index (from 0->%d) for %d node: "%(numberOfNodes-1,node)))
+			if(tableOfEvents[node][1]>numberOfNodes):
+				tableOfEvents[node][1]=int(input("[DATA] Enter a valid preceding node index (from 0->%d) for %d node: "%(numberOfNodes-1,node)))
+
+#This function checks if the graph has conitnuity
+def graph_continuity(tableOfEvents,numberOfNodes):
+	for node in range(0,(numberOfNodes)):
+		tableOfEvents[int(tableOfEvents[node][2])][3]=1
 
 #Explains the relation between the nodes
 def graph_explainer(tableOfEvents,numberOfNodes):
@@ -61,15 +68,19 @@ def graph_explainer(tableOfEvents,numberOfNodes):
 		print("[INFO] Node: %d follows Node: %d"%(node,int(tableOfEvents[node][1])+1))
 
 #Defining the event time between the nodes, relative in nature
-def rel_events(a,numberOfNodes):
+def rel_events(tableOfEvents,numberOfNodes):
 	for node in range(1,numberOfNodes):
 		tableOfEvents[node][2]=input("[DATA] Enter the relative time for %d and %d event to take place:"%(node,tableOfEvents[node][1]))
 
 #Calculating the absolute time between with respect to the starting point
 def abs_events(tableOfEvents,numberOfNodes):
-	for node in range(1,numberOfNodes):
-		node=int(tableOfEvents[node][1])
-		tableOfEvents[node][3]=tableOfEvents[node][2]+tableOfEvents[node][3]
+	for current_node in range(1,numberOfNodes):
+		#Here, the leader node is node leading upto the current node.
+		'''The abs_time will be the relative time between current_node and the leader_node
+		and the abs_time of the leader_node
+		'''
+		leader_node=int(tableOfEvents[leader_node][1])
+		tableOfEvents[current_node][3]=tableOfEvents[current_node][2]+tableOfEvents[leader_node][3]
 
 #To find the last node in the network
 def last_node(tableOfEvents,numberOfNodes):
@@ -87,12 +98,12 @@ def graph_builder(tableOfEvents,processGraph,numberOfNodes):
 	processGraph.node('0')
 	processGraph.attr('node',shape='circle')
 	for node in range(1,len(tableOfEvents)):
-		processGraph.edge(str(int(tableOfEvents[node][1])),str(int(node)),label=("Forward Pass Time: "+str(int(tableOfEvents[node][3]))))
+		processGraph.edge(str(int(tableOfEvents[node][1])),str(int(node)),label=("Forward Pass Time: "+str(int(tableOfEvents[node][2]))))
 		
 		#Backward time not yet implemented
 		#g.edge(str(int(node)),str(int(a[node][0])),label="Backward Pass Time: "+str(a[node][3]))
 
-#This function uses 
+#This function uses the processGraph and plots the graph for visualization.
 def graph_plotter(processGraph):
 	processGraph.view()
 	print('\n')
@@ -107,33 +118,36 @@ def graph_plotter(processGraph):
 		a[events_1][3]=np.amax(b)
 	print(b)'''
 
-#n is a variable to store the number of nodes involved in the network
+#n is a variable to store the number of nodes involved in the network.
 numberOfNodes=number_of_nodes()
 print("\n")
 
-#Declaring the numpy array here for entering the nodes
+#Declaring the numpy array here for entering the nodes.
 tableOfEvents=np.zeros((numberOfNodes,5))
 
-#Defines the relation of connectivity of the nodes
+#Defines the relation of connectivity of the nodes.
 event_relation(tableOfEvents,numberOfNodes)
 print("\n")
 
-#Explains the relations between the nodes for verification
+#Checking the continuity of the graph here.
+graph_continuity(tableOfEvents,numberOfNodes)
+
+#Explains the relations between the nodes for verification.
 graph_explainer(tableOfEvents,numberOfNodes)
 print("\n")
 
-#Function provides the number of events between nodes, in total
+#Function provides the number of events between nodes, in total.
 number_of_events(tableOfEvents,numberOfNodes)
 print("\n")
 
-#Function to enter the time elapsed in carrying out the events
+#Function to enter the time elapsed in carrying out the events.
 rel_events(tableOfEvents,numberOfNodes)
 
-#Function to evaluate the absolute time in execution of the events
+#Function to evaluate the absolute time in execution of the events.
 abs_events(tableOfEvents,numberOfNodes)
 print("\n")
 
-#Prints the last node in the network
+#Prints the last node in the network.
 j=last_node(tableOfEvents,numberOfNodes)
 print("\n")
 
